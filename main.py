874911c0bb00fbcd644d19569b4cdbe95c8f94fe
Arghya Das <arghyadasproject@gmail.com>
2023-05-04 12:00:46 +0530

    Upgraded version more fine-tuned

--- main.py
+++ main.py
@@ -3,31 +3,63 @@ from langchain.chat_models import ChatOpenAI
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.chains import RetrievalQA
 from langchain.vectorstores import Chroma
-from langchain.embeddings import TensorflowHubEmbeddings
 from langchain.document_loaders import PyPDFDirectoryLoader
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.embeddings import HuggingFaceEmbeddings
 import os
 from dotenv import load_dotenv
+
 load_dotenv()
 
 openai_api_key = os.getenv('OPENAI_API_KEY')
 chatbot_data_path = os.getenv('CHATBOT_DATA_DIR')
+chroma_dir = os.getenv('CHROMA_DIR')
 
-loader = PyPDFDirectoryLoader(chatbot_data_path)
-documents = loader.load()
+try:
+    loader = PyPDFDirectoryLoader(chatbot_data_path)
+    documents = loader.load()
+except Exception as e:
+    st.error(f"Error loading documents: {e}")
+    raise
 
 text_splitter = RecursiveCharacterTextSplitter(
-    chunk_size=1000, chunk_overlap=0)
+    chunk_size=1000, chunk_overlap=50)
 texts = text_splitter.split_documents(documents)
-embeddings = TensorflowHubEmbeddings()
-docsearch = Chroma.from_documents(texts, embeddings)
-qa = RetrievalQA.from_chain_type(llm=ChatOpenAI(verbose=False, temperature=0), chain_type="stuff",
-                                 retriever=docsearch.as_retriever(search_kwargs={"k": 1}), return_source_documents=True)
+persist_directory = chroma_dir
+embeddings = OpenAIEmbeddings()
+
+if os.path.exists(persist_directory):
+    try:
+        print("Loading from Existing Embeddings")
+        docsearch = Chroma(persist_directory=persist_directory,
+                           embedding_function=embeddings)
+        qa = RetrievalQA.from_chain_type(llm=ChatOpenAI(verbose=False, temperature=0.2), chain_type="stuff",
+                                         retriever=docsearch.as_retriever(search_kwargs={"k": 2}), return_source_documents=True)
+    except Exception as e:
+        st.error(f"Error searching from existing Embeddings, please wait: {e}")
+        raise
+else:
+    try:
+        print("Creating New Embeddings")
+        docsearch = Chroma.from_documents(
+            documents=texts, embedding=embeddings, persist_directory=persist_directory)
+        docsearch.persist()
+        qa = RetrievalQA.from_chain_type(llm=ChatOpenAI(verbose=False, temperature=0.2), chain_type="stuff",
+                                         retriever=docsearch.as_retriever(search_kwargs={"k": 2}), return_source_documents=True)
+    except Exception as e:
+        st.error(f"Error creating new embeddings, please wait: {e}")
+        raise
 
 st.title("Document Chatbot")
 st.write("This is a chatbot that can answer questions about a document.")
 query = st.text_input("Enter your query here")
+
 if query:
-    result = qa({"query": query})
-    st.success(result["result"])
-    st.json({"Source: ": os.path.basename(result["source_documents"][0].metadata["source"]),
-             "Page Number: ": result["source_documents"][0].metadata["page"]})
+    try:
+        result = qa({"query": query})
+        st.success(result["result"])
+        st.json({"Source: ": os.path.basename(result["source_documents"][0].metadata["source"]),
+                 "Page Number: ": result["source_documents"][0].metadata["page"]})
+    except Exception as e:
+        st.error(f"Error getting answer: {e}")
+        raise
